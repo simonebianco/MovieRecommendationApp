435169333e75a27f75ce53dd948c8199cb99ab8c
simonebianco <82176085+simonebianco@users.noreply.github.com>
2022-08-07 13:06:22 +0200

    Update home.py

--- home.py
+++ home.py
@@ -24,7 +24,7 @@ st.title('Movie Recommendation App')
 
 # descrizione app
 st.subheader('App')
-text_app = 'The application is built using the Streamlit module with Python and is intended to provide recommendations on 10 movies with higher similarity than the one entered by the user. The final dataset was processed by merging the individual csvs, and only the first 2000 highest rated films were kept. The data were then pre-processed and vectorized to allow calculation of the cosine similarity among the 2000 films presents which is used as a measure for recommendation'
+text_app = 'The application is built using the Streamlit module with Python and is intended to provide recommendations on 10 movies with higher similarity than the one entered by the user. The final dataset was processed by merging the individual csvs, and only the first 3000 highest rated films were kept. The data were then pre-processed and vectorized to allow calculation of the cosine similarity among the 3000 films presents which is used as a measure for recommendation'
 st.write(text_app)
 
 # descrizione dati
